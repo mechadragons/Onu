6e1ec54728865b29df5124d7cde9c1fb5b6e16b9
mechadragons <vgstever@outlook.com>
2023-04-26 13:07:01 -0700

    Added unit test for card class

--- cardTest.java
+++ cardTest.java
@@ -1,7 +1,12 @@
 class CardTest {
     public static void main(String args[]) {
-        Card testCard = new Card("red", "7");
+        CardTest test = new CardTest();
+        test.createCard();
     }
 
-    
+    void createCard() {
+        Card testCard = new Card("red", "7");
+        System.out.println("Card color: " + testCard.color + " (should be red)");
+        System.out.println("Card symbol: " + testCard.symbol + " (should be 7)");
+    }
 }
