cde9d8deb9b7b23bbe2af6475af4b0ba0ffde91b
ksolly12 <123200816+ksolly12@users.noreply.github.com>
2023-05-01 10:52:20 -0700

    Updated Menu, has a title screen and rules option

--- UnoMenu.java
+++ UnoMenu.java
@@ -4,52 +4,99 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UnoMenu extends JFrame {
-    private JTextField[] nameFields;
     private JButton startButton;
+    private JButton rulesButton;
+    private JFrame titleScreen;
 
     public UnoMenu() {
         super("Uno Game Menu");
 
         // Set up the frame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 200);
+        setSize(400, 300);
         setLocationRelativeTo(null);
 
         // Create the main panel
         JPanel mainPanel = new JPanel(new BorderLayout());
 
-        // Create the player name panel
-        JPanel playerPanel = new JPanel(new GridLayout(9, 1));
-        JLabel label = new JLabel("Enter player names:");
-        playerPanel.add(label);
+        // Create the title panel
+        JPanel titlePanel = new JPanel();
+        JLabel titleLabel = new JLabel("UNO");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 48));
+        titlePanel.add(titleLabel);
+        mainPanel.add(titlePanel, BorderLayout.NORTH);
 
-        nameFields = new JTextField[8];
-        for (int i = 0; i < 8; i++) {
-            nameFields[i] = new JTextField(20);
-            playerPanel.add(nameFields[i]);
-        }
-        mainPanel.add(playerPanel, BorderLayout.CENTER);
-
-        // Create the start button
+        // Create the button panel
+        JPanel buttonPanel = new JPanel();
         startButton = new JButton("Start Game");
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                startGame();
+                openMainMenu();
             }
         });
-        mainPanel.add(startButton, BorderLayout.SOUTH);
+        buttonPanel.add(startButton);
 
-        // Add the main panel to the frame
-        getContentPane().add(mainPanel);
+        rulesButton = new JButton("View Rules");
+        rulesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewRules();
+            }
+        });
+        buttonPanel.add(rulesButton);
 
-        // Pack and set the frame visible
-        pack();
-        setVisible(true);
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        add(mainPanel);
+    }
+
+    private void viewRules() {
+        // UnoRulesPopUp.main(new String[0]);
+    }
+
+    private void openMainMenu() {
+        JFrame mainMenu = new JFrame("Uno Game Menu");
+
+        // Create the player name panel
+        JPanel playerPanel = new JPanel(new GridLayout(8, 1));
+        JLabel label = new JLabel("Enter player names:");
+        playerPanel.add(label);
+
+        JTextField[] nameFields = new JTextField[8];
+        for (int i = 0; i < 8; i++) {
+            nameFields[i] = new JTextField(20);
+            playerPanel.add(nameFields[i]);
+        }
+
+        // Create the start button
+        JButton startButton = new JButton("Start Game");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startGame();
+                mainMenu.dispose(); // Close the menu
+            }
+        });
+        playerPanel.add(startButton);
+
+        mainMenu.getContentPane().add(playerPanel);
+        mainMenu.setSize(300, 400);
+        mainMenu.setLocationRelativeTo(null);
+        mainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainMenu.setVisible(true);
+
+        titleScreen.dispose(); // Close the title screen
     }
 
     private void startGame() {
         // Get the player names
+        JFrame mainMenu = new JFrame("Uno Game Menu");
+        JTextField[] nameFields = new JTextField[8];
+        for (int i = 0; i < 8; i++) {
+            nameFields[i] = new JTextField(20);
+        }
+
         String[] playerNames = new String[8];
         for (int i = 0; i < 8; i++) {
             playerNames[i] = nameFields[i].getText().trim();
@@ -77,8 +124,11 @@ public class UnoMenu extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                UnoMenu menu = new UnoMenu();
+                UnoMenu titleScreen = new UnoMenu();
+                titleScreen.setVisible(true);
+                titleScreen.titleScreen = titleScreen; // Save the reference to the title screen
             }
         });
     }
 }
+
